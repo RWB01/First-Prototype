9360420c0b3446d18023adefd1c5146fd4307042
tennobyte <slowpok.fake@gmail.com>
2017-06-17 17:41:19 +1000

    FP-17:Test_sessions interface, now able to add group to student

--- app/assets/javascripts/test_sessions.js
+++ app/assets/javascripts/test_sessions.js
@@ -1,6 +1,11 @@
-
-function addTest(id){
-	
+function addTest(algorithm_name, input_variables){
+	str = "";
+	str += "<div class=\"test\">";
+	str += "<div class=\"algorithm_title\">" + algorithm_name + "</div>"
+	str += "<div class=\"input_variables\">" + input_variables + "</div>";
+	str += "<div class=\"delete_button\"><button class=\"delete_test\">Удалить тест</button></div>";
+	str += "</div>";
+	return str;
 }
 
 document.addEventListener("turbolinks:load", function() {
@@ -15,6 +20,20 @@ document.addEventListener("turbolinks:load", function() {
 				$(this).data("status","opened");
 				$("#algorithm_menu_" + temp_algorithm_id).show(500);
 			}
+		});
+
+		$("#submit_test_session").click(function(){
+
+		});
+
+		$(document).on('click','.add_new_test', function(){
+			name = $("#algorithm_name_"+this.id.substring(13)).html();
+			variable = $("input[name=input_type_" + this.id.substring(13) + "]:checked", '#variable_type_form_' + this.id.substring(13)).val(); 
+			$(".tests_list").append(addTest(name,variable));
+		});
+
+		$(document).on('click','.delete_test',function(){
+			$(this).parent().parent().remove();
 		});
 	});
 });
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
     get :remove_role, on: :member
     get :add_discipline, on: :member
     get :remove_discipline, on: :member
+    get :add_group, on: :member
+    get :remove_group, on: :member
   end
 
   resources :roles
@@ -30,7 +32,6 @@ Rails.application.routes.draw do
 
   resources :algorithms do
     post :change_options, on: :member
-
   end
 
   resources :community
